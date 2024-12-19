c82e9a94a68783b944bf1d8078b971cb720d3495
Siva-M-U <siva.mu@juspay.in>
2024-12-20 01:14:29 +0530

    Add src folder and other project files

---
+++ src/pages/index.js
@@ -0,0 +1,45 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import * as styles from "../components/index.module.css"
+
+
+
+
+
+const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
+
+const IndexPage = () => (
+  <Layout>
+    <div className={styles.textCenter}>
+      <StaticImage
+        src="../images/example.png"
+        loading="eager"
+        width={64}
+        quality={95}
+        formats={["auto", "webp", "avif"]}
+        alt=""
+        style={{ marginBottom: `var(--space-3)` }}
+      />
+      <h1>
+        Welcome to <b>My Project 1</b>
+      </h1>
+      <p>I sucessfully completed my react designer course</p>
+      <p> Video 1 Completed..</p>
+      <Link to="/page-2/">Go to page 2</Link>
+      
+    </div>
+  </Layout>
+)
+
+/**
+ * Head export to define metadata for the page
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
+ */
+export const Head = () => <Seo title="Home" />
+
+export default IndexPage
